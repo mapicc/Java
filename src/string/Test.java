ae6f750e018555063343bf9b03d3101fcf558363
zhaoguanjun <773216251@qq.com>
2016-03-20 22:24:13 +0800

    测试ReetrantLock

---
+++ src/string/Test.java
@@ -0,0 +1,12 @@
+package string;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReentrantLock lock=new ReentrantLock();
+	}
+
+}
