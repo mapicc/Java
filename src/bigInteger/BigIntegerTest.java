b053a2dd43d2b92dc2bd8da0023defee7d5801c8
zhaoguanjun <773216251@qq.com>
2016-03-27 20:15:35 +0800

    大数相乘

---
+++ src/bigInteger/BigIntegerTest.java
@@ -0,0 +1,14 @@
+package bigInteger;
+
+import java.math.BigInteger;
+
+public class BigIntegerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BigInteger i=new BigInteger("1234568755214556221125522");
+		BigInteger j=new BigInteger("2255512222445522241452222");
+		System.out.println(i.multiply(j));
+	}
+
+}
