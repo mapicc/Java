bd7b7590c09d1a153c283d0ecf14a15cade102bb
zhaoguanjun <773216251@qq.com>
2016-04-03 22:08:32 +0800

    测试自动封装厢以及Integer.cache

---
+++ src/test.java
@@ -0,0 +1,13 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i1=127,i2=127,i3=128,i4=128;
+		System.out.println(i1==i2);
+		System.out.println(i1.equals(i2));
+		System.out.println(i3==i4);
+		System.out.println(i3.equals(i4));
+	}
+
+}
