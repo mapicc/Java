b586cc72893d062d99cc3e42954a807570fa4262
zhaoguanjun <773216251@qq.com>
2016-04-13 21:06:34 +0800

    NIO

---
+++ src/nio/NIOTest.java
@@ -0,0 +1,22 @@
+package nio;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+import java.nio.channels.InterruptibleChannel;
+
+public class NIOTest {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		ByteBuffer buffer = ByteBuffer.allocate(48);
+		RandomAccessFile f = new RandomAccessFile("/file.txt", "rw");
+		FileChannel in = f.getChannel();
+		int bytesRead = in.read(buffer);
+	}
+
+}
