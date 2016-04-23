a3031c680aa36bc92aa5c53fe752801df6ff7277
zhaoguanjun <773216251@qq.com>
2016-04-23 22:34:44 +0800

    本工程中二叉树的一般定义

--- src/utilTree/TreeNode.java
+++ src/utilTree/TreeNode.java
@@ -1,10 +1,10 @@
 package utilTree;
 
 public class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-	TreeNode(int val){
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode(int val){
 		this.val = val;
 	}
 }
