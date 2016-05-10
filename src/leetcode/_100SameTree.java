26fc1348a880fb7142ba35c33d9f4eb60d91ab5b
mapic <773216251@qq.com>
2016-05-10 16:12:17 +0800

    判断二叉树是否相等

---
+++ src/leetcode/_100SameTree.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class _100SameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q){
+		if(p == null && q == null)
+			return true;
+		if(p != null && q != null){
+			if(p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right))
+				return true;
+		}
+		return false;
+	}
+}
