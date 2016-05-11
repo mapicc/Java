4e17a05669cac4380f5541387a2a4addb842302a
mapic <773216251@qq.com>
2016-05-11 16:31:04 +0800

    两种方法删除链表倒数第N个节点

--- src/leetcode/_19RemoveNthNodeFromEndOfList.java
+++ src/leetcode/_19RemoveNthNodeFromEndOfList.java
@@ -23,4 +23,21 @@ public class _19RemoveNthNodeFromEndOfList {
 		q.next = q.next.next;
 		return head;
 	}
+	
+	//更简便的方法
+	public ListNode removeNthFromEnd_2(ListNode head, int n){
+		ListNode slow, fast, dummy = new ListNode(0);
+		slow = head;
+		fast = head;
+		dummy.next = head;
+		for(int i = 0;i < n + 1; i++){
+			fast = fast.next;
+		}
+		while(fast != null){
+			fast = fast.next;
+			slow = slow.next;
+		}
+		slow.next = slow.next.next;
+		return dummy.next;
+	}
 }
