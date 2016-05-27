3a47168b182341f92a1c95c6f2d9222892736d28
zhaoguanjun <773216251@qq.com>
2016-05-27 16:34:41 +0800

    container With Most Water

---
+++ src/leetcode/_11ContainerWithMostWater.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class _11ContainerWithMostWater {
+	public int maxArea(int[] height){
+		int max = 0, i = 0, j = height.length - 1;
+		while(i < j){
+			max = Math.max(max, (j - i) * (height[i] < height[j] ? height[i] : height[j]));
+		}
+		return max;
+	}
+}
