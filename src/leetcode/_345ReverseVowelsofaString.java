c9a76e610c73d1d3929899187afa320d41506829
zhaoguanjun <zhaoguanjun@172.28.50.35>
2016-07-07 16:10:21 +0800

    反转字符串中的元音字符3

---
+++ src/leetcode/_345ReverseVowelsofaString.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class _345ReverseVowelsofaString {
+
+	public static String reverseVowels(String s) {
+		//try 2
+		if(s == null)
+			return s;
+		int i = 0, j = s.length() - 1;
+		char[] ch = s.toCharArray();
+		while(i < j){
+			char ci = ch[i];
+			char cj = ch[j];
+			if(ci == 'a' || ci == 'e' || ci == 'i' || ci == 'o' || ci == 'u' || ci == 'A' || ci == 'E' || ci == 'I' || ci == 'O' || ci == 'U'){
+				if(cj == 'a' || cj == 'e' || cj == 'i' || cj == 'o' || cj == 'u' || cj == 'A' || cj == 'E' || cj == 'I' || cj == 'O' || cj == 'U'){
+					ch[i] = cj;
+					ch[j] = ci;
+					j--;
+					i++;
+				}else
+				    j--;
+			}else{  
+			    i++;
+			}
+		} 
+		return new String(ch);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(reverseVowels("aA"));
+	}
+
+}
