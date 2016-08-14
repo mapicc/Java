abe53f4e5d84782cedb78178465cf8f9a465d4fd
zhaoguanjun <773216251@qq.com>
2016-08-14 21:05:57 +0800

    华为笔试题: 数独游戏

--- src/Main4.java
+++ src/Main4.java
@@ -31,10 +31,47 @@ public class Main4 {
                     }
                 }
             }
-            System.out.println(row);
-            System.out.println(col);
-            System.out.println(squ);
+            
+            dfs(data, row, col, squ, 0);
+            
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    if (j != 8) {
+                        System.out.print(data[i][j] + " ");
+                    }else {
+                        System.out.println(data[i][j]);
+                    }
+                }
+            }
         }
+        scanner.close();
+    }
 
+    private static boolean dfs(int[][] data, List<HashSet<Integer>> row, List<HashSet<Integer>> col, List<HashSet<Integer>> squ, int index) {
+        if (index == 81)
+            return true;
+        int m = index / 9;
+        int n = index % 9;
+        int k = m / 3 * 3 + n / 3;
+
+        if (data[m][n] != 0) {
+            return dfs(data, row, col, squ, index + 1);
+        }else {
+            for (int i = 1; i <= 9; i++) {
+                if (!row.get(m).contains(i) && !col.get(n).contains(i) && !squ.get(k).contains(i)){
+                    data[m][n] = i;
+                    row.get(m).add(i);
+                    col.get(n).add(i);
+                    squ.get(k).add(i);
+                    if (dfs(data, row, col, squ, index + 1))
+                        return true;
+                    data[m][n] = 0;
+                    row.get(m).remove(i);
+                    col.get(n).remove(i);
+                    squ.get(k).remove(i);
+                }
+            }
+            return false;
+        }
     }
 }
