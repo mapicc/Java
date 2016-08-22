f4fe27b01b68383100223606b9997c0efee11be9
zhaoguanjun <773216251@qq.com>
2016-08-22 22:11:38 +0800

    第一个非重复字符

---
+++ src/leetcode/_387FirstUniqueCharacterinaString.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by guanjun on 2016/8/22.
+ */
+public class _387FirstUniqueCharacterinaString {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i)))
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            else
+                map.put(s.charAt(i), 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+}
