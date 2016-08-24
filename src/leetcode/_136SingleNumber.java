a0023f12e9241ad1dec640f1aec9c8a838426480
zhaoguanjun <773216251@qq.com>
2016-08-24 13:39:09 +0800

    数组中成单元素

--- src/leetcode/_136SingleNumber.java
+++ src/leetcode/_136SingleNumber.java
@@ -6,8 +6,8 @@ package leetcode;
 public class _136SingleNumber {
     public int singleNumber(int[] nums) {
         int ret = nums[0];
-        for (int i = 0; i < nums.length - 1; i++) {
-            ret = nums[i] ^ nums[i + 1];
+        for (int i = 1; i < nums.length; i++) {
+            ret = nums[i] ^ ret;
         }
         return ret;
     }
