93b0e35b326593c30651120a96909e737ada3612
zhaoguanjun <773216251@qq.com>
2016-08-28 16:59:29 +0800

    优先级队列：通过Object[]数组实现平衡二叉堆，初始容量为11，不够是扩容，小于64扩容两倍，否则扩容一半。Iterator遍历元素结果位置，对头至队尾排列通过元素的比较器Comparator

--- src/priorityQueue/PriorityQueueExample.java
+++ src/priorityQueue/PriorityQueueExample.java
@@ -12,15 +12,22 @@ public class PriorityQueueExample {
         Person p1 = new Person("a", 11);
         Person p2 = new Person("b", 12);
         Person p3 = new Person("A", 9);
+        Person p4 = new Person("c", 17);
         Queue<Person> queue = new PriorityQueue<>(((o1, o2) -> o1.age - o2.age));
         queue.offer(p1);
         queue.offer(p2);
         queue.offer(p3);
+        queue.offer(p4);
 
         System.out.println("-------通过迭代器遍历-------");
         Iterator<Person> iterator = queue.iterator();
+        int count = 0;
         while (iterator.hasNext()) {
+            count++;
             System.out.println(iterator.next());
+            if (count == 2)
+//                queue.offer(new Person("e", 90));
+                iterator.remove();
         }
 
         /**
