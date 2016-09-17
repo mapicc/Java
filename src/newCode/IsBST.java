f7724d6623d05bc92a3c143d71f45b77416c1922
zhaoguanjun <773216251@qq.com>
2016-09-17 15:18:30 +0800

    判断二叉树是否是搜索二叉树、是否为完全二叉树

--- src/newCode/IsBST.java
+++ src/newCode/IsBST.java
@@ -2,11 +2,38 @@ package newCode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by zhaoguanjun on 2016/9/17.
  */
 public class IsBST {
+    public static boolean isCBT(TreeNode head) {
+        if (head == null)
+            return true;
+        TreeNode tmp = null;
+        TreeNode l = null;
+        TreeNode r = null;
+        boolean leaf = false;
+        boolean ret = true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(head);
+        while (!queue.isEmpty()) {
+            tmp = queue.poll();
+            l = tmp.left;
+            r = tmp.right;
+            if ((leaf && (l != null || r != null)) || (l == null && r != null))
+                ret = false;
+            if (l != null)
+                queue.offer(l);
+            if (r != null)
+                queue.offer(r);
+            else
+                leaf = true;
+        }
+        return ret;
+    }
+
     public static boolean isBST(TreeNode head) {
         if (head == null)
             return true;
@@ -45,8 +72,9 @@ public class IsBST {
         root.left.right = new TreeNode(3);
         root.right = new TreeNode(6);
         root.right.left = new TreeNode(5);
-        root.right.right = new TreeNode(7);
+        //root.right.right = new TreeNode(7);
 
         System.out.println(isBST(root));
+        System.out.println(isCBT(root));
     }
 }
