2eefd246908e494f102b867a0d346a0b42a9e656
zhaoguanjun <773216251@qq.com>
2016-09-17 22:08:47 +0800

    将一个数通过/2或+1或-1方式以最快的方式变为1

---
+++ src/leetcode/_397IntegerReplacement.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * Created by zhaoguanjun on 2016/9/17.
+ */
+public class _397IntegerReplacement {
+    public int integerReplacement(int n) {
+        long N = n;
+        int count = 0;
+        while(N != 1) {
+            if((N & 1) == 0)
+                N >>= 1;
+            else{
+                long small = (N & (N - 1));
+                long big = (N & (N + 1));
+                if((small & (small - 1)) <= (big & (big - 1)))
+                    N -= 1;
+                else
+                    N += 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
