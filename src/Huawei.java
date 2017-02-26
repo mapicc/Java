64e5afe966de4268cd4809f108082eed2ea457ce
mapicc <773216251@qq.com>
2017-02-26 20:44:43 +0800

    华为软件训练营1

--- src/Huawei.java
+++ src/Huawei.java
@@ -1,10 +1,15 @@
-import java.util.Date;
+import java.util.Scanner;
 
 /**
  * Created by zhaoguanjun on 2016/8/13.
  */
 public class Huawei {
     public static void main(String[] args) {
-        System.out.println(new Date());
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine().trim();
+            int ind = str.lastIndexOf(' ');
+            System.out.println(str.length() - ind - 1);
+        }
     }
 }
