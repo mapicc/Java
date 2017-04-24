5f1536492e8f995c0ae547bed65258c1eb1f8259
mapicc <773216251@qq.com>
2017-04-24 20:03:03 +0800

    训练营
    Signed-off-by: mapicc <773216251@qq.com>

--- src/Itheima.java
+++ src/Itheima.java
@@ -2,11 +2,17 @@
  * Created by mapicc on 2017/4/24.
  */
 public class Itheima {
-    public boolean has12(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == 1 && arr[i + 1] == 2)
-                return true;
-        }
+    public static boolean has12(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+            if (arr[i] == 1)
+                for (int j = arr.length - 1; j > i; j--)
+                    if (arr[j] == 2)
+                        return true;
         return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2};
+        System.out.println(has12(arr));
     }
 }
